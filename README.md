0560aa696ca977d84cddbee8ba8ae43fa17b35fe
Joshua Harms <joshua@nozzlegear.com>
2017-02-14 20:42:45 -0600

    Converted to TypeScript

--- README.md
+++ README.md
@@ -1,28 +1,54 @@
-# react-flexifit
-A React component to make non-responsive elements, (such as iframes, canvas, etc) or components responsive.
+# react-flexifit-ts
+A React component to make non-responsive elements, (such as iframes, canvas, etc) or components responsive with full TypeScript definitions.
 
-Inspired by fitvids.
+Based off of https://github.com/LeisureLink-FE/react-flexifit, which was inspired by fitvids.
+
+## Installation
+
+Install via [Yarn](https://yarnpkg.com):
+
+```bash
+yarn add react-flexifit-ts
+```
+
+Or via [NPM](https://npmjs.com/package/react-flexifit-ts):
+
+```bash
+npm i react-flexifit-ts --save
+```
+
+## Importing
+
+Import this package via ES6's default import:
+
+```js
+import Flexifit from "react-flexifit-ts";
+```
+
+Or import it via Node's require:
+
+```js
+const Flexifit = require("react-flexifit-ts").default;
+```
 
 ## Usage
 
 ### aspectRatio
 ```
-import Flexifit from 'react-flexifit';
-
 <Flexifit aspectRatio={1.5} >
   <iframe src="https://www.youtube.com/embed/1g6QJ5TfA7w"/>
 </Flexifit>
 ```
+
 Flexifit will ensure it's child element maintains the specified aspectRatio no matter the screen size.
 
 ### height/width
 ```
-import Flexifit from 'react-flexifit';
-
 <Flexifit height={200} width={250} >
   <iframe src="https://www.youtube.com/embed/1g6QJ5TfA7w"/>
 </Flexifit>
 ```
+
 Flexifit will calculate the appropriate aspect ratio and maintain that calculated aspectRatio.
 
 ## Considerations
---
+++ index.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import choke = require('lodash/throttle');
+import { Component, Children, cloneElement } from 'react';
+
+const isPositiveFinite = (num) => (num > 0) && Number.isFinite(num);
+
+export interface IProps extends React.Props<any> {
+	aspectRatio: number;
+	width: number;
+	height: number;
+	throttle: number;
+}
+
+export interface IState {
+	flexer?: any;
+	ratio?: number;
+	width?: number;
+	height?: number;
+}
+
+export default class FlexiFrame extends Component<IProps, IState> {
+	constructor(props: IProps, context) {
+		super(props, context);
+
+		const {aspectRatio, width, height, throttle = 100, children} = props;
+		let flexer;
+
+		try {
+			flexer = Children.only(children);
+		} catch (e) {
+			const message = [
+				`It looks as though you tried to include more than one child to Flexifit.`,
+				`Flexifit can only manage one non-responsive element at a time.`,
+				`Try wrapping additional non-responsive elements in their own Flexifit component.`,
+			];
+			
+			throw message.join(` `);
+		}
+
+		const { style, ...flexerProps } = flexer.props;
+		this.throttledResize = choke(this.handleResize, throttle).bind(this);
+
+		this.state = {
+			ratio: aspectRatio || (isPositiveFinite(width / height) ? width / height : 1),
+			flexer: cloneElement(flexer, {
+				...flexerProps,
+				key: 1,
+				onLoad: this.throttledResize,
+				style: {
+					...(style || {}),
+					top: 0,
+					left: 0,
+					width: '100%',
+					height: '100%',
+					position: 'absolute'
+				}
+			}, [flexer.children]),
+		};
+	}
+
+	throttledResize: () => void;
+
+	flexWrapper: HTMLDivElement;
+
+	componentDidMount() {
+		window.addEventListener('resize', this.throttledResize);
+		this.throttledResize();
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.throttledResize);
+	}
+
+	handleResize() {
+		const width = parseInt(window.getComputedStyle(this.flexWrapper).width) || 0;
+		this.setState({ width });
+	}
+
+	render() {
+		const { width, ratio } = this.state;
+		const height = (width || 1) / ratio;
+		const style: React.CSSProperties = {width: "100%", height: `${height}px`, position: `relative`};
+
+		return (
+			<div style={style} ref={(f) => this.flexWrapper = f}>
+				{[this.state.flexer]}
+			</div>
+		);
+	}
+}
